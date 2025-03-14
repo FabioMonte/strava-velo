3a88ffc52881b16fed10d1dcc588eaab2298e6a0
FabioMonte <fabiomonte82@gmail.com>
2025-03-14 18:25:32 +0000

    Remove folder

--- src/app/environment.ts
+++
@@ -1,9 +0,0 @@
-﻿export const environment = {
-  production: false,
-  stravaClientId: '92165',
-  stravaClientSecret: 'd9d6382546c46d0f276d514c457afc158986168f',
-  stravaRedirectUri: 'http://localhost:4200/auth/callback',
-  stravaAuthUrl: 'https://www.strava.com/oauth/authorize',
-  stravaTokenUrl: 'https://www.strava.com/oauth/token',
-  stravaApiUrl: 'https://www.strava.com/api/v3'
-};
